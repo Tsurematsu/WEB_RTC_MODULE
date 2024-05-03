b4bb5e7a207d5eaac666fe822883489bf1286f47
Tsurematsu <daniel120802@gmail.com>
2024-05-03 08:36:29 -0500

    conexión peer actualizada

--- complementary/src/client/sokete.js
+++ complementary/src/client/sokete.js
@@ -3,23 +3,57 @@ export default async function sokete({ url="/", name="Juan", room="room1"}) {
     url = String(url);
     name = String(name);
     room = String(room);
+    const local_Log = console.log;
+    const iceServers = await ReadJSON("/cli/iceServers.json");
+    const peerClients = {};
+    const createPeerClient = (client) => {
+        const peerClient = new RTCPeerConnection({iceServers})
+        const chat = peerClient.createDataChannel('chat');
+        // ---------------------------------------------------------
+        chat.addEventListener('open', () => {
+            let randomNumber = Math.floor(Math.random() * 1000);
+            local_Log("Send: Hola k tal:" + randomNumber)
+            chat.send("Hola k tal:" + randomNumber);
+        });
+        
+        peerClient.addEventListener('datachannel', (event) => {
+            const canal = event.channel;
+            canal.addEventListener('message', (input) => {
+                local_Log(input.data);
+            })
+        });
+
+        peerClient.onicecandidate = (event) => {if (event.candidate) {
+            socket.send({event:"candidate", data:event.candidate, to:client}); 
+        }};
+        return peerClient;
+    };
     const io = (await import("./socket.io.esm.min.js")).default;
-    // const iceServers = await ReadJSON("/cli/iceServers.json");
     const socket = io(url);
-    socket.on("connect", () => {
-        console.log("ID:", socket.id);
-        socket.emit("join", room);
-    });
-    
+    socket.on("connect", () => {socket.emit("join", room);});
     socket.on("join", (client) => {
-        let num_random = Math.floor(Math.random() * 1000);
-        let valueData = "Hola: " + num_random;
-        console.log("join: ", client, " send value :" + valueData);
-        // socket.emit("offer", valueData, client);
-        socket.send({event:"offer", data:valueData, to:client})
+        let tempPeerClient = createPeerClient(client);
+        tempPeerClient.createOffer().then((offer) => {
+            tempPeerClient.setLocalDescription(offer);
+            socket.send({event:"offer", data:offer, to:client});
+        });
+        peerClients[client] = tempPeerClient;
+    });
+    socket.on("offer", (offer, sender) => {
+        let temPeerClient = createPeerClient(sender);
+        temPeerClient.setRemoteDescription(new RTCSessionDescription(offer));
+        temPeerClient.createAnswer().then((answer) => {
+            temPeerClient.setLocalDescription(answer);
+            socket.send({event:"answer", data:answer, to:sender});
+        });
+        peerClients[sender] = temPeerClient;
     });
 
-    socket.on("offer", (offer, sender)=>{
-        console.log("offer: ", offer);
-    })
+    socket.on("answer", (answer, sender) => {
+        peerClients[sender].setRemoteDescription(new RTCSessionDescription(answer));
+    });
+
+    socket.on("candidate", (candidate, sender) => {
+        peerClients[sender].addIceCandidate(new RTCIceCandidate(candidate));
+    });
 }
